63ac530f8a6c9e841c9a87519d5e9f568b47fca6
alcalos9 <calderon.osorio.alejandro@gmail.com>
2022-12-27 10:16:27 -0300

    Initial commit

---
+++ src/main/java/com/example/man/demoMan/DemoManApplication.java
@@ -0,0 +1,13 @@
+package com.example.man.demoMan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/man/demoMan/DemoManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.man.demoMan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
